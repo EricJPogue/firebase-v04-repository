9305ea274e1895bdf62160ea35368050a8b5c677
Eric Pogue <epogue@epogue.com>
2025-10-29 14:49:15 -0500

    Add initial Firebase files.

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -2,15 +2,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
 
-// Your Firebase configuration
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
-  };
+  apiKey: "AIzaSyBEnBljmW8eWB1xgUszLdvZfSv5OqEeFFY",
+  authDomain: "fir-v04-project.firebaseapp.com",
+  projectId: "fir-v04-project",
+  storageBucket: "fir-v04-project.firebasestorage.app",
+  messagingSenderId: "346998630573",
+  appId: "1:346998630573:web:b4f4e1ec87732fe0cebe24"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
